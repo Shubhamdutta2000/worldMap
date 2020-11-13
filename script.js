666aa1d45cdae290dc8a0a7ff3db9868b6827939
Shubhamdutta2000 <dondutta2000@gmail.com>
2020-11-13 20:01:58 +0530

    Map :)

---
+++ script.js
@@ -0,0 +1,44 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2h1YmhhbWR1dHRhMjAwMCIsImEiOiJja2hmczE3bGUwN3FkMzNwOXFieTF4c3pzIn0.hg5VzFtkWwC-6KrvgDonGg';
+var map = new mapboxgl.Map({
+     container: 'map',
+     style: 'mapbox://styles/mapbox/streets-v11',
+     center: [78.9629, 20.5937], // starting position
+     zoom: 5
+});
+
+// Add geolocate control to the map. (for current location)
+map.addControl(
+     new mapboxgl.GeolocateControl({
+          positionOptions: {
+               enableHighAccuracy: true
+          },
+          trackUserLocation: true
+     })
+);
+
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+// driving direction
+map.addControl(
+     new MapboxDirections({
+          accessToken: mapboxgl.accessToken
+     }),
+     'top-left'
+);
+
+
+var layerList = document.getElementById('menu');
+var button = layerList.getElementsByTagName('button');
+
+console.log(button.length);
+
+function switchLayer(layer) {
+     var layerId = layer.target.id;
+     map.setStyle('mapbox://styles/mapbox/' + layerId);
+}
+
+for (var i = 0; i < button.length; i++) {
+     button[i].onclick = switchLayer;
+}
